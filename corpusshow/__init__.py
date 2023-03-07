718edfc7c41a800c59e07e30a3275fbdf75512bf
parkminwoo <parkminwoo1991@gmail.com>
2023-03-07 15:26:03 +0900

    hot fix: build 0.1.4

--- corpusshow/__init__.py
+++ corpusshow/__init__.py
@@ -2,4 +2,4 @@ from corpusshow.corpus_cluster import CorpusCluster
 
 __all__ = ['CorpusCluster']
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="corpusshow",
-    version="0.1.3",
+    version="0.1.4",
     author="parkminwoo",
     author_email="parkminwoo1991@gmail.com",
     description="Corpus-Show makes it easier and faster to visualize corpus through sentence embedding of corpus.",
@@ -16,5 +16,6 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=[
         "sentence-transformers",
-        "quickshow"
+        "quickshow",
+        "tokenizer"
     ])
