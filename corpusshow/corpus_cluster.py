a8fc85aa0387e16d9e46416f3d2001b5a696733d
parkminwoo <parkminwoo1991@gmail.com>
2023-03-09 17:33:42 +0900

    feat: CorpusCluster.show

--- corpusshow/corpus_cluster.py
+++ corpusshow/corpus_cluster.py
@@ -1,8 +1,10 @@
-from sentence_transformers import SentenceTransformer
-from sklearn.cluster import KMeans
 import pandas as pd
 import quickshow as qs
 from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sentence_transformers import SentenceTransformer
+from sklearn.cluster import KMeans
 
 
 class CorpusCluster:
@@ -89,3 +91,130 @@ class CorpusCluster:
         
         return return_df
 
+
+    def show(self, true_label_col: str, save_plot_path: str) -> pd.DataFrame:
+        if true_label_col == 'cluster':
+            if 'cluster' not in self.df.columns:
+                self.df = self.get_df_cluster()
+            if 'embedded_sentence' not in self.df.columns:
+                self.df['embedded_sentence'] = [self.embed(x) for x in tqdm(self.df[self.target_col])]
+            qs.vis_tsne2d(self.df, 'embedded_sentence', 'cluster', True, None)
+            qs.vis_tsne3d(self.df, 'embedded_sentence', 'cluster', True, None)
+            qs.vis_pca(self.df, 'embedded_sentence', 'cluster', 3, True, None)
+
+            fig = plt.figure(figsize=plt.figaspect(1))
+            ax = fig.add_subplot(2,2,2, projection='3d')
+            x, y, z = self.df['tsne-3d-1'], self.df['tsne-3d-2'], self.df['tsne-3d-3']
+            ax.scatter3D(x, y, z)
+            plt.title("T-SNE 3D", fontsize=15)
+            for s in self.df['cluster'].unique():
+                ax.scatter(self.df['tsne-3d-1'][self.df['cluster']==s], 
+                        self.df['tsne-3d-2'][self.df['cluster']==s], 
+                        self.df['tsne-3d-3'][self.df['cluster']==s], 
+                        label=s)
+            ax.set_xlabel('tsne-3d-1')
+            ax.set_ylabel('tsne-3d-2')
+            ax.set_zlabel('tsne-3d-3')
+            ax.legend()
+
+            plt.subplot(2,2,3)
+            plt.title('PCA 2D', fontsize=15)
+            sns.scatterplot(
+                    x="PC1", y="PC2",
+                    hue='cluster',
+                    palette=sns.color_palette("Set2", len(self.df['cluster'].unique())),
+                    data=self.df,
+                    legend="full"
+                    )
+
+            plt.subplot(2,2,1)
+            plt.title("T-SNE 2D", fontsize=15)
+            sns.scatterplot(
+                x="tsne-2d-1", y="tsne-2d-2",
+                hue='cluster',
+                palette=sns.color_palette("Set2", len(self.df['cluster'].unique())),
+                data=self.df,
+                legend="full"
+                )
+
+            ax = fig.add_subplot(2,2,4, projection='3d')
+            plt.title("PCA 3D", fontsize=15)
+            fig = plt.figure(figsize=plt.figaspect(1))
+            for s in self.df['cluster'].unique():
+                ax.scatter(self.df['PC1'][self.df['cluster']==s], 
+                        self.df['PC2'][self.df['cluster']==s], 
+                        self.df['PC3'][self.df['cluster']==s], 
+                        label=s)
+            ax.set_xlabel('PC1')
+            ax.set_ylabel('PC2')
+            ax.set_zlabel('PC3')
+            ax.legend()
+
+            if save_plot_path is not None:
+                plt.savefig(save_plot_path, dpi=300, bbox_inches='tight')
+
+            plt.show()
+        
+        else:
+            if 'embedded_sentence' not in self.df.columns:
+                self.df['embedded_sentence'] = [self.embed(x)for x in tqdm(self.df[self.target_col])]
+            self.df = qs.vis_tsne2d(self.df, 'embedded_sentence', true_label_col, True, None)
+            self.df = qs.vis_tsne3d(self.df, 'embedded_sentence', true_label_col, True, None)
+            self.df = qs.vis_pca(self.df, 'embedded_sentence', true_label_col, 3, True, None)
+
+            fig = plt.figure(figsize=plt.figaspect(1))
+            ax = fig.add_subplot(2,2,2, projection='3d')
+            x, y, z = self.df['tsne-3d-1'], self.df['tsne-3d-2'], self.df['tsne-3d-3']
+            ax.scatter3D(x, y, z)
+            plt.title("T-SNE 3D", fontsize=15)
+            for s in self.df[true_label_col].unique():
+                ax.scatter(self.df['tsne-3d-1'][self.df[true_label_col]==s], 
+                        self.df['tsne-3d-2'][self.df[true_label_col]==s], 
+                        self.df['tsne-3d-3'][self.df[true_label_col]==s], 
+                        label=s)
+            ax.set_xlabel('tsne-3d-1')
+            ax.set_ylabel('tsne-3d-2')
+            ax.set_zlabel('tsne-3d-3')
+            ax.legend()
+
+            plt.subplot(2,2,3)
+            plt.title('PCA 2D', fontsize=15)
+            sns.scatterplot(
+                    x="PC1", y="PC2",
+                    hue=true_label_col,
+                    palette=sns.color_palette("Set2", len(self.df[true_label_col].unique())),
+                    data=self.df,
+                    legend="full"
+                    )
+
+            plt.subplot(2,2,1)
+            plt.title("T-SNE 2D", fontsize=15)
+            sns.scatterplot(
+                x="tsne-2d-1", y="tsne-2d-2",
+                hue=true_label_col,
+                palette=sns.color_palette("Set2", len(self.df[true_label_col].unique())),
+                data=self.df,
+                legend="full"
+                )
+
+            ax = fig.add_subplot(2,2,4, projection='3d')
+            plt.title("PCA 3D", fontsize=15)
+            fig = plt.figure(figsize=plt.figaspect(1))
+            for s in self.df[true_label_col].unique():
+                ax.scatter(self.df['PC1'][self.df[true_label_col]==s], 
+                        self.df['PC2'][self.df[true_label_col]==s], 
+                        self.df['PC3'][self.df[true_label_col]==s], 
+                        label=s)
+            ax.set_xlabel('PC1')
+            ax.set_ylabel('PC2')
+            ax.set_zlabel('PC3')
+            ax.legend()
+
+            if save_plot_path is not None:
+                plt.savefig(save_plot_path, dpi=300, bbox_inches='tight')
+
+            plt.show()
+
+        
+            
+        return self.df
