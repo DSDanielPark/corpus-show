61e08a84cb1df4cb0f91fe1289e1da7113374c1c
parkminwoo <parkminwoo1991@gmail.com>
2023-03-13 17:01:22 +0900

    build: 0.1.8 - show method, quickshow and plt ver.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="corpusshow",
-    version="0.1.7",
+    version="0.1.8",
     author="parkminwoo",
     author_email="parkminwoo1991@gmail.com",
     description="Corpus-Show makes it easier and faster to visualize corpus through sentence embedding of corpus.",
@@ -17,6 +17,6 @@ setuptools.setup(
     install_requires=[
     "transformers", "tokenizer",
         "sentence-transformers",
-        "quickshow",
+        "quickshow>=0.1.12",
         "matplotlib>==3.4.3"
     ])
